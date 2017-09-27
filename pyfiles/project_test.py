481070a97ae457cbdbeea1956157cbf34dc428df
Subbu Rao <tech@subbu.me>
2017-09-27 00:15:56 -0700

    pyfiles added

---
+++ pyfiles/project_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 23:16:49 2017
+
+@author: subbu
+"""
+y_start_stop = [300, 700] # Min and max in y to search in slide_window()
+
+image = mpimg.imread('test_images/test4.jpg')
+draw_image = np.copy(image)
+
+# Uncomment the following line if you extracted training
+# data from .png images (scaled 0 to 1 by mpimg) and the
+# image you are searching is a .jpg (scaled 0 to 255)
+image = image.astype(np.float32)/255
+
+windows = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop, 
+                    xy_window=(64, 64), xy_overlap=(0.5, 0.5))
+
+hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space, 
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)                       
+
+window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)                    
+
+plt.imshow(window_img)
